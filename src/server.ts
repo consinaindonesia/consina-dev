afc8c8475cf37979753bfb23767cd6a53c13e5d4
consinaindonesia <consinaindonesia@gmail.com>
2026-05-21 15:57:12 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import { createServer } from "@tanstack/react-start/server";
 import start from "./start";
 
-const server = createServer(start);
-server.listen(3005);
+export default {
+  fetch(request: Request) {
+    return start.fetch(request);
+  },
+};
